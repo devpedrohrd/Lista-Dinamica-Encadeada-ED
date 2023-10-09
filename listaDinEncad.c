1561d073c62adb8d45cca1804a0e996db348a1db
Pedrohrd87 <pedrohrd87@gmail.comanode>
2023-10-09 10:27:10 -0300

    .

--- listaDinEncad.c
+++ listaDinEncad.c
@@ -17,9 +17,14 @@ typedef struct elemento Elem;
 
 Lista *cria_lista()
 {
+    // Aloca memória para a estrutura Lista (ponteiro para o primeiro elemento da lista)
     Lista *li = (Lista *)malloc(sizeof(Lista));
+
+    // Verifica se a alocação de memória foi bem-sucedida
     if (li != NULL)
-        *li = NULL; // Lista aponta para null
+        *li = NULL; // Inicializa a lista como vazia (ponteiro aponta para NULL)
+
+    // Retorna o ponteiro para a lista (pode ser NULL se a alocação de memória falhou)
     return li;
 }
 
@@ -123,21 +128,17 @@ int insere_lista_final(Lista *li, struct numeros num)
 int insere_lista_ordenada(Lista *li, struct numeros num)
 {
     if (li == NULL)
-        return 0;
+        return 0; // Retorna 0 se a lista for nula
 
     // Aloca memória para um novo elemento da lista
     Elem *no = (Elem *)malloc(sizeof(Elem));
-    if (no == NULL)
-    {
-        return 0; // Retorna 0 se a alocação de memória falhar
-    }
 
     // Define os dados do novo elemento como o número fornecido
     no->dados = num;
+    no->prox = NULL; // Inicializa o próximo como NULL
 
     if (lista_vazia(li))
     { // Se a lista estiver vazia, insere no início
-        no->prox = (*li);
         *li = no;
         return 1; // Retorna 1 para indicar sucesso na inserção
     }
@@ -149,8 +150,21 @@ int insere_lista_ordenada(Lista *li, struct numeros num)
         while (atual != NULL && atual->dados.numero < num.numero)
         {
             ant = atual;         // O elemento anterior recebe o elemento atual
-            atual = atual->prox; // O elemento atual avança para o
+            atual = atual->prox; // O elemento atual avança para o próximo
         }
+
+        if (ant == NULL)
+        { // Insere no início da lista
+            no->prox = *li;
+            *li = no;
+        }
+        else
+        { // Insere no meio ou final da lista
+            no->prox = ant->prox;
+            ant->prox = no;
+        }
+
+        return 1; // Retorna 1 para indicar sucesso na inserção
     }
 }
 
@@ -163,9 +177,9 @@ int remove_lista_inicio(Lista *li)
     if ((*li) == NULL) // se o inicio da lista aponta para null retorna 0(é vazia);
         return 0;
 
-    Elem *no = *li; // elemento criado se torna o inicio da lista
+    Elem *no = *li; // elemento criado aponta para o inicio da lista
 
-    *li = no->prox; // inicio da lista recebe o proximo elemento depois do (no);
+    *li = no->prox; // atualiza o ponteiro de inicio da lista para o proximo elemento;
 
     free(no); // e o elemento que foi criado e era o inicio da lista é excluido;
     return 1;
@@ -207,6 +221,8 @@ int remove_lista_final(Lista *li)
     {
         ant = no;      // A variável "ant" recebe o nó atual
         no = no->prox; // A variável "no" avança para o próximo nó
+
+        // este while se repete até encontrar o ultimo elemento
     }
 
     if (no == (*li))
@@ -242,11 +258,14 @@ int remove_lista(Lista *li, int num)
     if (no == NULL)
         return 0; // Retorna 0 se o elemento não foi encontrado na lista
 
-    if (no == *li)      // Se o nó a ser removido for o primeiro
+    if (no == *li)
+    {                   // Se o nó a ser removido for o primeiro
         *li = no->prox; // Atualiza o ponteiro de início para o próximo elemento
+    }
     else
+    {
         ant->prox = no->prox; // Atualiza o ponteiro do nó anterior para onde o "no" aponta
-
+    }
     free(no); // Libera a memória do nó removido
 
     return 1; // Retorna 1 para indicar sucesso na rem
@@ -258,7 +277,7 @@ int consulta_lista_pos(Lista *li, int pos, struct numeros *num)
     if (li == NULL || pos <= 0)
         return 0; // Retorna 0 para indicar falha
 
-    // Inicializa um ponteiro para percorrer a lista
+    // Inicializa um ponteiro que aponta para o inicio da lista
     Elem *no = *li;
 
     // Inicializa um contador para rastrear a posição atual na lista
